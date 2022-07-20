f096cd7bebfbca61793f94356fb66724328daae3
ptaylor <paul.e.taylor@me.com>
2022-07-20 13:58:22 -0700

    only build the Appimage

--- config/forge.config.js
+++ config/forge.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   packagerConfig: {},
   makers: [
-    {name: '@electron-forge/maker-zip', platforms: ['linux']},
-    {name: '@electron-forge/maker-deb', config: {}},
+    // {name: '@electron-forge/maker-zip', platforms: ['linux']},
+    // {name: '@electron-forge/maker-deb', config: {}},
     {name: 'electron-forge-maker-appimage', config: {}},
     // { name: '@electron-forge/maker-rpm', config: {} }
   ],
