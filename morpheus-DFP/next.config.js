d29f2a2901495dda18ece9f1d72e10f62b2eef80
Ajay Thorve <ajaythorve1994@gmail.com>
2022-11-08 11:00:52 -0800

    webpack management of rapidsai packages no longer required

--- morpheus-DFP/next.config.js
+++ morpheus-DFP/next.config.js
@@ -1,29 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // reactStrictMode: true,
+  reactStrictMode: true,
   experimental: { esmExternals: true },
   eslint: {
     ignoreDuringBuilds: true,
   },
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    // Note: we provide webpack above so you should not `require` it
-    // Perform customizations to webpack config
-    // config.plugins.push(new webpack.IgnorePlugin({ resourceRegExp: /.*?\.node/ig }))
-    if (isServer) {
-      config.externals.push({
-        "@rapidsai/core": "@rapidsai/core",
-        "@rapidsai/cuda": "@rapidsai/cuda",
-        "@rapidsai/webgl": "@rapidsai/webgl",
-        "@rapidsai/deck.gl": "@rapidsai/deck.gl",
-        "@rapidsai/rmm": "@rapidsai/rmm",
-        "@rapidsai/cudf": "@rapidsai/cudf",
-        "apache-arrow": "apache-arrow",
-      });
-    }
-    // console.log(require('util').inspect({ isServer, config }, false, Infinity, true));
-    // Important: return the modified config
-    return config;
-  },
 };
 
-module.exports = { ...nextConfig };
+module.exports = nextConfig;
